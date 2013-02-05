118b39db16c94d3cb8a5bc1e56545646b1b4cd26
Mike Weber <mike@weberapps.com>
2013-02-05 14:36:55 -0600

    allow for more options to trigger achievements

--- plugins/over_achiever_listener.rb
+++ plugins/over_achiever_listener.rb
@@ -4,11 +4,13 @@ class OverAchieverListener
   include Cinch::Plugin
 
   $help_messages << "achieve: <number> <message>   Generate an achievement with <number> points"
+  $help_messages << "achievement: <number> <message>   Generate an achievement with <number> points"
+  $help_messages << "achievement unlocked: <number> <message>   Generate an achievement with <number> points"
 
   listen_to :channel
 
   def listen(m)
-    if matches = m.message.match(/^achieve: (\d*)(\D.*)/)
+    if matches = m.message.match(/^(?:ach[ie]{2}ve(?:ment)?|ach[ie]{2}vement unlocked): (\d*)(\D.*)/)
       points = matches[1].empty? ? 5 : matches[1]
       msg = matches[2].strip
       if badge = OverAchiever.generate_badge(points, msg)
